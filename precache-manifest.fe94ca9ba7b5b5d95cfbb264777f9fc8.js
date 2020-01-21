8a8c63425b502df84470e57e61b5ade0b24fec3b
javidrashidlone@airasia.com <javidrashidlone@airasia.com>
2020-01-21 18:02:54 +0530

    Updates

---
+++ precache-manifest.fe94ca9ba7b5b5d95cfbb264777f9fc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2ed2e15518e1eeed197a84fd7ffec2",
+    "url": "/texada/index.html"
+  },
+  {
+    "revision": "bf3bb64a5e449e1b299e",
+    "url": "/texada/static/css/2.23c648bd.chunk.css"
+  },
+  {
+    "revision": "361eb72f580bcff5bbe0",
+    "url": "/texada/static/css/main.aaf8a68d.chunk.css"
+  },
+  {
+    "revision": "bf3bb64a5e449e1b299e",
+    "url": "/texada/static/js/2.40e3535c.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/texada/static/js/2.40e3535c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "361eb72f580bcff5bbe0",
+    "url": "/texada/static/js/main.be0c0c74.chunk.js"
+  },
+  {
+    "revision": "bc541b30ef5c6854188b",
+    "url": "/texada/static/js/runtime-main.bc8014f7.js"
+  }
+]);
